41bac78ecd54d64686416e8e1618220b23417f95
twan65 <cx.twan65@gmail.com>
2020-10-23 22:06:40 +0900

    TOP画面のボタン追加

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,29 +1,42 @@
 import "package:flutter/material.dart";
 
-// 1-2. 탭 화면 (각 화면 import)
+// 1-2. タブ画面 (各画面のimport)
 
-// 1-2. 탭 화면 (Stateless -> Stateful)
+// 1-2. タブ画面 (Stateless -> Stateful)
 class MainPage extends StatelessWidget {
-  // 1-2. 탭 화면 (_selectedTabIndex 변수 옮김)
-  // 1-2. 탭 화면 (탭 인덱스 설정)
+  // 1-2. タブ画面 (_selectedTabIndex変数を移動)
+  // 1-2. タブ画面 (タブインデックス設定)
 
-  // 1-2. 탭 화면 (createState 함수 추가)
+  // 1-2. タブ画面 (createState関数追加)
 
-  // 1-2. 탭 화면 (build() 함수를 _MainState로 옮김)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // 1-1. 상단화면 (제목 수정)
-        title: Text('Flutter'),
-        // 1-1. 상단화면 (좌측 버튼 추가)
-
-        // 1-1. 상단화면 (우측 팝업 버튼 및 이벤트 추가)
-
+        // TOP画面のタイトル
+        title: Text('Movie'),
+        // ポップアップボタン及びイベント追加
+        actions: <Widget>[
+          PopupMenuButton<int>(
+              icon: Icon(Icons.sort),
+              onSelected: (value) {
+                if(value == 0) print("予約数順");
+                else if(value == 1) print("キュレーション");
+                else print("最新順");
+              },
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(value: 0, child: Text("予約数順")),
+                  PopupMenuItem(value: 1, child: Text("キュレーション")),
+                  PopupMenuItem(value: 2, child: Text("最新順")),
+                ];
+              }
+          ),
+        ],
       ),
-      // 1-2. 탭 화면 (List, Grid Widget 연동)
+      // 1-2. List, Grid Widget連動
       body: Center(child: Text("Hello Flutter")),
-      // 1-2. 탭 화면 (bottomNavigationBar 추가)
+      // 1-2. bottomNavigationBar追加
     );
   }
 }
