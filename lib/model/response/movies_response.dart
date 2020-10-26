ffae3f57555b22302910115daa9464616e15dc7d
twan65 <cx.twan65@gmail.com>
2020-10-26 16:53:21 +0900

    Movieモデルの修正

--- lib/model/response/movies_response.dart
+++ lib/model/response/movies_response.dart
@@ -2,13 +2,13 @@ class MoviesResponse {
   int orderType;
   List<Movie> movies;
 
-  // 생성자
+  // コンストラクタ
   MoviesResponse({
     this.orderType,
     this.movies,
   });
 
-  // map 구조에서 새로운 MoviesResponse 객체를 생성
+  // map構造から新しいMoviesResponseオブジェクトを生成
   MoviesResponse.fromJson(Map<String, dynamic> json) {
     if (json['movies'] != null) {
       movies = [];
@@ -16,36 +16,30 @@ class MoviesResponse {
         movies.add(Movie.fromJson(movie));
       });
     }
+
     orderType = json['order_type'];
   }
 
-  // MoviesResponse 객체를 map 구조로 변환
+  // MoviesResponseからmapに変換
   Map<String, dynamic> toMap() {
     final map = Map<String, dynamic>();
     if (movies != null) {
       map['movies'] = movies.map((movie) => movie.toMap()).toList();
     }
+
     map['order_type'] = orderType;
     return map;
   }
 }
 
 class Movie {
-  // 영화 제목
-  String title;
-  // 사용자 평점
-  int userRating;
-  // 관람 등급 (0 : 전체 / 12 : 12세 / 15 : 15세 / 19 :19세)
-  int grade;
-  // 예매 순위
-  int reservationGrade;
-  // 영화 고유 id
   String id;
-  // 개봉일
+  String title;
+  int userRating;
+  int grade;
+  int reservationGrade;
   String date;
-  // 포스터 이미지 섬네일 주소
   String thumb;
-  // 예매율
   double reservationRate;
 
   Movie(
