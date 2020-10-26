3ebbf2bd3937293e390efabde7ebf39b740386cf
twan65 <cx.twan65@gmail.com>
2020-10-26 14:06:31 +0900

    ListItemの共通化

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -9,57 +9,64 @@ class ListPage extends StatelessWidget {
     // 初期リスト
     return ListView(
       children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(12.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Image.network(
-                  '',
-                height: 120),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          'test1',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 8),
-                        Text("12"),
-                        ],
-                    ),
-                        SizedBox(height: 10),
-
-                        Row(
-                          children: <Widget>[
-                            Text('点数：139'),
-                            SizedBox(width: 10),
-                            Text('予約順位：1'),
-                            SizedBox(width: 10),
-                            Text('予約率：35.5')
-                          ],
-                        ),
-                        SizedBox(height: 10),
-                        Text('公開日：2020年10月24日')
-                      ],
-                    ),
-              )
-            ],
-          ),
-        ),
+        _buildDummyItem(),
+        _buildDummyItem(),
+        _buildDummyItem(),
+        _buildDummyItem(),
+        _buildDummyItem(),
       ]
     );
   }
 
-// 1-3. 리스트 화면 (고정 더미 데이터)
+  // ListItem共通化
+  Widget _buildDummyItem() {
+    return Container(
+      padding: EdgeInsets.all(12.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.network(
+              '',
+              height: 120),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'test1',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Text("12"),
+                  ],
+                ),
+                SizedBox(height: 10),
+
+                Row(
+                  children: <Widget>[
+                    Text('点数：139'),
+                    SizedBox(width: 10),
+                    Text('予約順位：1'),
+                    SizedBox(width: 10),
+                    Text('予約率：35.5')
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text('公開日：2020年10月24日')
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
 // 1-3. 리스트 화면 (동적 데이터 호출1)
 
