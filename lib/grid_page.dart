def758c1310b4e1d58e38b6d365a71bfeb5aaf0d
twan65 <cx.twan65@gmail.com>
2020-10-27 13:22:54 +0900

    gird画面の枠作成

--- lib/grid_page.dart
+++ lib/grid_page.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
 
-// 1-3. Grid 리스트 화면 (임포트)
+import 'model/data/dummys_repository.dart';
+import 'model/response/movies_response.dart';
 
 class GridPage extends StatelessWidget {
-  // 1-4. Grid 리스트 화면 (동적 데이터 추가)
+  final List<Movie> movies = DummysRepository.loadDummyMovies();
 
   @override
   Widget build(BuildContext context) {
-    // 1-4. Grid 리스트 화면 (GridView 구축)
-    return Center(child: Text("grid"));
+    return GridView.builder(
+        scrollDirection: Axis.vertical,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: (9 / 16),
+        ),
+        itemCount: movies.length,
+        itemBuilder: (context, index) =>
+            _buildGrideItem(context, index: index),
+    );
   }
 
-// 1-4. Grid 리스트 화면 (Grid 아이템 화면 구축)
+  Widget _buildGrideItem(BuildContext context, {@required int index}) {
+    Movie movie = movies[index];
 
-// 1-4. Grid 리스트 화면 (관람 등급 이미지 버튼 함수 생성)
+    return Container(
+
+    );
+  }
+
+  // グレードイメージ取得
+  Widget _buildGradeImage(int grade) {
+    switch (grade) {
+      case 0:
+        return Image.asset("assets/ic_allages.png");
+      case 12:
+        return Image.asset("assets/ic_12.png");
+      case 15:
+        return Image.asset("assets/ic_15.png");
+      case 19:
+        return Image.asset("assets/ic_19.png");
+      default:
+        return null;
+    }
+  }
 }
