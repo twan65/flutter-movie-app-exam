bed81dc0c0f26c2a1b3c2fac54874962aa962eb6
twan65 <cx.twan65@gmail.com>
2020-10-27 16:33:07 +0900

    Grid画面の詳細画面遷移のイベント追加

--- lib/grid_page.dart
+++ lib/grid_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:padak_starter/detail_page.dart';
 
 import 'model/data/dummys_repository.dart';
 import 'model/response/movies_response.dart';
@@ -23,44 +24,51 @@ class GridPage extends StatelessWidget {
   Widget _buildGrideItem(BuildContext context, {@required int index}) {
     Movie movie = movies[index];
 
-    return Container(
-      padding: EdgeInsets.all(8),
-      child: Column(
-        children: <Widget>[
-          Expanded(
-              child: Stack(
-                alignment: Alignment.topRight,
-                children: <Widget>[
-                  Image.network(
-                    movie.thumb,
-                    fit: BoxFit.fill,
-                    width: double.infinity,
-                    height: double.infinity,
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    child: _buildGradeImage(movie.grade),
-                  )
-                ],
-              ),
-          ),
-          SizedBox(height: 8),
-          FittedBox(
-            child: Text(
-              movie.title,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+    return InkWell(
+      child: Container(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+                child: Stack(
+                  alignment: Alignment.topRight,
+                  children: <Widget>[
+                    Image.network(
+                      movie.thumb,
+                      fit: BoxFit.fill,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      child: _buildGradeImage(movie.grade),
+                    )
+                  ],
+                ),
+            ),
+            SizedBox(height: 8),
+            FittedBox(
+              child: Text(
+                movie.title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 8),
-          Text(
-            '${movie.reservationGrade}上（${movie.userRating}）/ ${movie.reservationRate}%'),
-          SizedBox(height: 8),
-          Text('${movie.date}'),
-        ],
+            SizedBox(height: 8),
+            Text(
+              '${movie.reservationGrade}上（${movie.userRating}）/ ${movie.reservationRate}%'),
+            SizedBox(height: 8),
+            Text('${movie.date}'),
+          ],
+        ),
       ),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (_) => DetailPage(movies[index].id)),
+        );
+      },
     );
   }
 
