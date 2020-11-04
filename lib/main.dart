1caa11accea1a78ccaafd9d3496db6a56e5fc648
twan65 <cx.twan65@gmail.com>
2020-11-04 21:48:36 +0900

    メインビルド画面をログイン画面に変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:padak_starter/main_page.dart';
+import 'package:padak_starter/login.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainPage(),
+      // home: MainPage(),
+      home: LoginPage(),
     );
   }
 }
