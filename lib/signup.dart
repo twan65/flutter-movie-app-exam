9025a1ca1312096e1d97c7c6406969b9400ff84c
twan65 <cx.twan65@gmail.com>
2020-11-06 19:42:02 +0900

    登録完了アラート追加

--- lib/signup.dart
+++ lib/signup.dart
@@ -8,11 +8,19 @@ class SignUpPage extends StatefulWidget {
   }
 }
 
+enum Answers{
+  OK,
+}
+
 // State作成
 class _SignUpPageState extends State<SignUpPage> {
 
   String _email;
   String _password;
+
+  String _value = '';
+  void _setValue(String value) => setState(() => _value = value);
+
 
   @override
   Widget build(BuildContext context) {
@@ -56,11 +64,37 @@ class _SignUpPageState extends State<SignUpPage> {
                 new RaisedButton(
                   child:
                   new Text('OK', style: new TextStyle(fontSize: 20.0, color: Colors.blue)),
-                  onPressed: () => {
-
-                  },
+                  onPressed: _onPressedSignUp,
                 ),
               ],
             )));
   }
+
+  // TODO
+  void _onPressedSignUp() {
+    print('sign up');
+    _showDialog();
+
+  }
+
+  // TODO
+  Future _showDialog() async {
+    var value = await showDialog(
+      context: context,
+      builder: (BuildContext context) => new AlertDialog(
+        title: new Text('案内'),
+        content: new Text('会員登録が正常に完了しました。'),
+        actions: <Widget>[
+          new SimpleDialogOption(child: new Text('OK'),onPressed: (){Navigator.pop(context, Answers.OK);},),
+        ],
+      ),
+    );
+
+    switch(value) {
+      case Answers.OK:
+        _setValue('OK');
+        break;
+    }
+  }
+
 }
